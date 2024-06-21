b9001d9c2b67851091fe6e6591d2f6649a41e5cb
mo7medk7 <m>
2024-06-21 19:23:40 +0300

    delete_task

--- lib/todo_tab/task_list/task_widget.dart
+++ lib/todo_tab/task_list/task_widget.dart
@@ -4,34 +4,54 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:provider/provider.dart';
 import 'package:fluttertoast/fluttertoast.dart';
-import '../../firebase_utils.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import '../../firebase_utils.dart'; // Adjust this as per your project structure
 import '../../model/task.dart';
 import '../../my_theme.dart';
 import '../../providers/app_config_provider.dart';
 import '../../providers/list_provider.dart';
 
-
 class TaskWidget extends StatefulWidget {
-  Task task;
+  final Task task;
 
   TaskWidget({required this.task});
 
   @override
-  State<TaskWidget> createState() => _TaskWidgetState();
+  _TaskWidgetState createState() => _TaskWidgetState();
 }
 
 class _TaskWidgetState extends State<TaskWidget> {
+  bool isAdmin = false; // Track admin status
+
+  @override
+  void initState() {
+    super.initState();
+    _checkAdminStatus();
+  }
+
+  Future<void> _checkAdminStatus() async {
+    // Check if current user is admin
+    User? user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      DocumentSnapshot<Map<String, dynamic>> snapshot =
+      await FirebaseFirestore.instance.collection('users').doc(user.uid).get();
+      if (snapshot.exists) {
+        setState(() {
+          isAdmin = snapshot.data()!['isAdmin'] ?? false; // Assuming 'isAdmin' is boolean in Firestore
+        });
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     var provider = Provider.of<AppConfigProvider>(context);
-
-    var Listprovider = Provider.of<ListProvider>(context);
-    ///   var authprovider = Provider.of<AuthhProvider>(context);
+    var listProvider = Provider.of<ListProvider>(context);
 
     return InkWell(
       onTap: () {
-        Navigator.pushNamed(context, EditTaskScreen.routeName,
-            arguments: widget.task);
+        Navigator.pushNamed(context, EditTaskScreen.routeName, arguments: widget.task);
       },
       child: Container(
         margin: EdgeInsets.all(12),
@@ -40,31 +60,25 @@ class _TaskWidgetState extends State<TaskWidget> {
             extentRatio: 0.30,
             motion: const ScrollMotion(),
             children: [
-              SlidableAction(
-                borderRadius: BorderRadius.circular(15),onPressed: (context) {
-
-                FirebaseUtils.deleteTaskFromFireStore(
-                    widget.task)
-                    .timeout(Duration(milliseconds: 500), onTimeout: () {
-                  showToastMessage("todo deleted successfully");
-                  print("todo deleted successfully");
-                  Listprovider.getAllTasksFromFireStore();
-                });
-              },
-                backgroundColor: MyTheme.redColor,
-                foregroundColor: MyTheme.whiteColor,
-                icon: Icons.delete,
-                label: AppLocalizations.of(context)!.delete,
-              ),
+              if (isAdmin) // Check if user is admin
+                SlidableAction(
+                  borderRadius: BorderRadius.circular(15),
+                  onPressed: (context) {
+                    _deleteTask();
+                  },
+                  backgroundColor: MyTheme.redColor,
+                  foregroundColor: MyTheme.whiteColor,
+                  icon: Icons.delete,
+                  label: AppLocalizations.of(context)!.delete,
+                ),
             ],
           ),
           child: Container(
             padding: EdgeInsets.all(10),
             decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15),
-                color: provider.isDarkMode()
-                    ? MyTheme.blueTask
-                    : MyTheme.blueTask),
+              borderRadius: BorderRadius.circular(15),
+              color: provider.isDarkMode() ? MyTheme.blueTask : MyTheme.blueTask,
+            ),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
@@ -76,55 +90,55 @@ class _TaskWidgetState extends State<TaskWidget> {
                   width: 5,
                 ),
                 Expanded(
-                    child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(
-                        widget.task.title ?? '',
-                        style: Theme.of(context).textTheme.titleSmall?.copyWith(
-                            color: widget.task.isDone!
-                                ? MyTheme.greenLight
-                                : MyTheme.primaryLightColor),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          widget.task.title ?? '',
+                          style: Theme.of(context).textTheme.titleSmall?.copyWith(
+                              color: widget.task.isDone!
+                                  ? MyTheme.greenLight
+                                  : MyTheme.primaryLightColor),
+                        ),
                       ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child:Row(children: [
-                        Text(widget.task.description ?? '',
-                            style: Theme.of(context).textTheme.titleSmall),
-                      ],)
-                    ),
-                  ],
-                )),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          widget.task.description ?? '',
+                          style: Theme.of(context).textTheme.titleSmall,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
                 InkWell(
                   onTap: () {
                     widget.task.isDone = !widget.task.isDone!;
-                    FirebaseUtils.editIsDone(
-                        widget.task);
+                    FirebaseUtils.editIsDone(widget.task);
                     setState(() {});
                   },
                   child: widget.task.isDone!
                       ? Text(
-                          "Done!",
-                          style: Theme.of(context)
-                              .textTheme
-                              .titleLarge!
-                              .copyWith(color: MyTheme.greenLight),
-                        )
+                    "Done!",
+                    style: Theme.of(context)
+                        .textTheme
+                        .titleLarge!
+                        .copyWith(color: MyTheme.greenLight),
+                  )
                       : Container(
-                          padding:
-                              EdgeInsets.symmetric(horizontal: 21, vertical: 7),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(15),
-                            color: MyTheme.primaryLightColor,
-                          ),
-                          child: Icon(
-                            Icons.check,
-                            size: 35,
-                            color: MyTheme.whiteColor,
-                          )),
+                    padding: EdgeInsets.symmetric(horizontal: 21, vertical: 7),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      color: MyTheme.primaryLightColor,
+                    ),
+                    child: Icon(
+                      Icons.check,
+                      size: 35,
+                      color: MyTheme.whiteColor,
+                    ),
+                  ),
                 )
               ],
             ),
@@ -132,12 +146,26 @@ class _TaskWidgetState extends State<TaskWidget> {
         ),
       ),
     );
-  }  void showToastMessage(String message) => Fluttertoast.showToast(
-      msg: message,
-      fontSize: 16,
-      gravity: ToastGravity.BOTTOM,
-      toastLength: Toast.LENGTH_SHORT,
-      backgroundColor: MyTheme.yelloColor,
-      textColor: MyTheme.blackColor);
+  }
 
+  void _deleteTask() {
+    if (isAdmin) {
+      FirebaseUtils.deleteTaskFromFireStore(widget.task)
+          .timeout(Duration(milliseconds: 500), onTimeout: () {
+        showToastMessage("Todo deleted successfully");
+        Provider.of<ListProvider>(context, listen: false).getAllTasksFromFireStore();
+      });
+    } else {
+      showToastMessage("Only admin can delete tasks");
+    }
+  }
+
+  void showToastMessage(String message) => Fluttertoast.showToast(
+    msg: message,
+    fontSize: 16,
+    gravity: ToastGravity.BOTTOM,
+    toastLength: Toast.LENGTH_SHORT,
+    backgroundColor: MyTheme.yelloColor,
+    textColor: MyTheme.blackColor,
+  );
 }
